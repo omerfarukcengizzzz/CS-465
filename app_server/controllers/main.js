02fbb431ef6a9e6bf2f736d43819528aba3fe9e7
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2025-09-24 20:36:27 -0700

    bug fix -> {main.js} : corrected title

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET Homepage */
 const index = (req, res) => {
-    res.render('index', { title: "Travlr Gateways" })
+    res.render('index', { title: "Travlr Getaways" })
 };
 
 module.exports = {
